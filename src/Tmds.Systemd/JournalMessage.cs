02fc978822848af305af6d985011a3618b16c653
Tom Deseyn <tom.deseyn@gmail.com>
2018-05-08 17:54:54 +0200

    Implement ServiceManager.Log

---
+++ src/Tmds.Systemd/JournalMessage.cs
@@ -0,0 +1,187 @@
+using System;
+using System.Buffers;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Tmds.Systemd
+{
+    /// <summary>Represents a structured log message.</summary>
+    public class JournalMessage : IDisposable
+    {
+        // If the buffer can't fit at least a character, the Encoder throws.
+        private const int MaximumBytesPerUtf8Char = 4;
+        private static Encoding s_encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+        [ThreadStatic]
+        private static JournalMessage s_cachedMessage;
+
+        private readonly List<ArraySegment<byte>> _data;
+        private readonly Encoder _encoder;
+        private byte[] _currentSegment;
+        private int _bytesWritten;
+
+        private JournalMessage()
+        {
+            _data = new List<ArraySegment<byte>>();
+            _encoder = s_encoding.GetEncoder();
+        }
+
+        /// <summary>Destructor.</summary>
+        ~JournalMessage()
+        {
+            Clear();
+        }
+
+        /// <summary>Obtain a cleared JournalMessage. The Message must be Disposed to return it.</summary>
+        public static JournalMessage Get()
+        {
+            JournalMessage message = s_cachedMessage;
+            if (message == null)
+            {
+                message = new JournalMessage();
+            }
+            else
+            {
+                s_cachedMessage = null;
+            }
+
+            return message;
+        }
+
+        /// <summary>Adds a field to the message.</summary>
+        public void Append(string name, object value)
+        {
+            // Field name
+            AppendString(name);
+
+            // Separator
+            AppendChar('\n');
+
+            // Field value
+            // Reserve space for length
+            EnsureCapacity(8);
+            Span<byte> valueLengthAt = CurrentRemaining;
+            _bytesWritten += 8;
+            // value
+            int bytesWritten = AppendString(value.ToString());
+            // Fill in length
+            BinaryPrimitives.WriteUInt64LittleEndian(valueLengthAt, (ulong)bytesWritten);
+
+            // Separator
+            AppendChar('\n');
+        }
+
+        private void Clear()
+        {
+            AppendCurrent();
+            foreach (var segment in _data)
+            {
+                ArrayPool<byte>.Shared.Return(segment.Array);
+            }
+        }
+
+        /// <summary>Returns the JournalMessage.</summary>
+        public void Dispose()
+        {
+            Clear();
+
+            if (s_cachedMessage == null
+            || (this._data.Count > s_cachedMessage._data.Count)) // Prefer caching longer _data
+            {
+                s_cachedMessage = this;
+            }
+        }
+
+        internal List<ArraySegment<byte>> GetData()
+        {
+            AppendCurrent();
+            return _data;
+        }
+
+        internal bool IsEmpty => _currentSegment == null;
+
+        private void AppendCurrent()
+        {
+            if (_currentSegment != null)
+            {
+                _data.Add(new ArraySegment<byte>(_currentSegment, 0, _bytesWritten));
+                _bytesWritten = 0;
+                _currentSegment = null;
+            }
+        }
+
+        private void AppendChar(char c)
+        {
+            if (c <= 127)
+            {
+                EnsureCapacity(1);
+
+                _currentSegment[_bytesWritten] = (byte)c;
+                _bytesWritten++;
+            }
+            else
+            {
+                _AppendMultiByteChar(c);
+            }
+        }
+
+        private unsafe void _AppendMultiByteChar(char value)
+        {
+            EnsureCapacity(MaximumBytesPerUtf8Char);
+            var destination = CurrentRemaining;
+
+            int bytesUsed = 0;
+            int charsUsed = 0;
+            fixed (byte* destinationBytes = &MemoryMarshal.GetReference(destination))
+            {
+                _encoder.Convert(&value, 1, destinationBytes, destination.Length, false, out charsUsed, out bytesUsed, out _);
+            }
+
+            _bytesWritten += bytesUsed;
+        }
+
+        private int AppendString(string s)
+        {
+            ReadOnlySpan<char> buffer = s.AsSpan();
+            int bytesWritten = 0;
+
+            while (buffer.Length > 0)
+            {
+                EnsureCapacity(MaximumBytesPerUtf8Char, desiredSize: buffer.Length);
+                Span<byte> destination = CurrentRemaining;
+
+                int bytesUsed = 0;
+                int charsUsed = 0;
+                unsafe
+                {
+                    fixed (char* sourceChars = &MemoryMarshal.GetReference(buffer))
+                    fixed (byte* destinationBytes = &MemoryMarshal.GetReference(destination))
+                    {
+                        _encoder.Convert(sourceChars, buffer.Length, destinationBytes, destination.Length, false, out charsUsed, out bytesUsed, out _);
+                    }
+                }
+                buffer = buffer.Slice(charsUsed);
+                _bytesWritten += bytesUsed;
+                bytesWritten += bytesUsed;
+            }
+
+            return bytesWritten;
+        }
+
+        private void EnsureCapacity(int minSize, int desiredSize = 0)
+        {
+            if (_currentSegment == null || (_currentSegment.Length - _bytesWritten) <  + minSize)
+            {
+                AppendCurrent();
+                desiredSize = Math.Max(desiredSize, 4096);
+                _currentSegment = ArrayPool<byte>.Shared.Rent(minimumLength: desiredSize);
+                _bytesWritten = 0;
+            }
+        }
+
+        private Span<byte> CurrentRemaining => new Span<byte>(_currentSegment, _bytesWritten, _currentSegment.Length - _bytesWritten);
+    }
+}
---
+++ src/Tmds.Systemd/ServiceManager.JournalSend.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Buffers;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace Tmds.Systemd
+{
+    using SizeT = System.UIntPtr;
+    using SSizeT = System.IntPtr;
+
+    /// <summary>
+    /// Log flags.
+    /// </summary>
+    [Flags]
+    public enum LogFlags
+    {
+        /// <summary>System is unusable.</summary>
+        Emergency = 1,
+        /// <summary>Action must be taken immediately.</summary>
+        Alert = 2,
+        /// <summary>Critical conditions.</summary>
+        Critical = 3,
+        /// <summary>Error conditions.</summary>
+        Error = 4,
+        /// <summary>Warning conditions.</summary>
+        Warning = 5,
+        /// <summary>Normal but significant conditions.</summary>
+        Notice = 6,
+        /// <summary>Informational.</summary>
+        Info = 7,
+        /// <summary>Debug-level messages.</summary>
+        Debug = 8
+    }
+
+    public partial class ServiceManager
+    {
+        private const string JournalSocketPath = "/run/systemd/journal/socket";
+        private const int MaxIovs = 100;
+        private const int EINTR = 4;
+
+        [ThreadStatic]
+        private static IOVector[] s_iovs;
+        [ThreadStatic]
+        private static GCHandle[] s_gcHandles;
+
+        private static Socket s_journalSocket;
+        private static bool s_hasJournal = true;
+
+        /// <summary>The syslog identifier string added to each message.</summary>
+        public static string SyslogIdentifier { get; set; } = "dotnet";
+
+        private static Socket GetJournalSocket()
+        {
+            if (!s_hasJournal)
+            {
+                return null;
+            }
+
+            if (s_journalSocket == null)
+            {
+                Socket journalSocket = Volatile.Read(ref s_journalSocket);
+                if (journalSocket == null)
+                {
+                    s_hasJournal = File.Exists(JournalSocketPath);
+                    if (s_hasJournal)
+                    {
+                        journalSocket = new Socket(AddressFamily.Unix, SocketType.Dgram, ProtocolType.Unspecified);
+                        journalSocket.Connect(new UnixDomainSocketEndPoint(JournalSocketPath));
+                        if (Interlocked.CompareExchange(ref s_journalSocket, journalSocket, null) != null)
+                        {
+                            journalSocket.Dispose();
+                        }
+                    }
+                }
+            }
+            return s_journalSocket;
+        }
+
+        /// <summary>
+        /// Submit a log entry to the journal.
+        /// </summary>
+        public static void Log(LogFlags flags, IReadOnlyList<KeyValuePair<string, object>> fields)
+        {
+            using (var message = JournalMessage.Get())
+            {
+                foreach (var field in fields)
+                {
+                    message.Append(field.Key, field.Value);
+                }
+                Log(flags, message);
+            }
+        }
+
+        /// <summary>
+        /// Submit a log entry to the journal.
+        /// </summary>
+        public static unsafe void Log(LogFlags flags, JournalMessage message)
+        {
+            if (message.IsEmpty)
+            {
+                return;
+            }
+
+            Socket socket = GetJournalSocket();
+            if (socket == null)
+            {
+                return;
+            }
+
+            int priority = (int)flags & 0xf;
+            if (priority != 0)
+            {
+                message.Append("PRIORITY", priority - 1);
+            }
+            if (SyslogIdentifier != null)
+            {
+                message.Append("SYSLOG_IDENTIFIER", SyslogIdentifier);
+            }
+
+            List<ArraySegment<byte>> data = message.GetData();
+            int dataLength = data.Count;
+            Span<IOVector> iovs = stackalloc IOVector[dataLength <= MaxIovs ? dataLength : 0];
+            if (iovs.IsEmpty)
+            {
+                iovs = GetHeapIovs(dataLength);
+            }
+            Span<GCHandle> handles = stackalloc GCHandle[dataLength <= MaxIovs ? dataLength : 0];
+            if (handles.IsEmpty)
+            {
+                handles = GetHeapHandles(dataLength);
+            }
+            for (int i = 0; i < data.Count; i++)
+            {
+                handles[i] = GCHandle.Alloc(data[i].Array, GCHandleType.Pinned);
+                iovs[i].Base = handles[i].AddrOfPinnedObject();
+                iovs[i].Length = new IntPtr(data[i].Count);
+            }
+            fixed (IOVector* pIovs = &MemoryMarshal.GetReference(iovs))
+            {
+                bool loop;
+                do
+                {
+                    loop = false;
+                    msghdr msg;
+                    msg.msg_iov = pIovs;
+                    msg.msg_iovlen = (SizeT)dataLength;
+                    int rv = sendmsg(socket.Handle.ToInt32(), &msg, 0).ToInt32();
+                    if (rv < 0)
+                    {
+                        var errno = Marshal.GetLastWin32Error();
+                        if (errno == EINTR)
+                        {
+                            loop = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error writing message to journal: errno={errno}.");
+                        }
+                    }
+                } while (loop);
+            }
+            for (int i = 0; i < handles.Length; i++)
+            {
+                handles[i].Free();
+            }
+        }
+
+        private static Span<IOVector> GetHeapIovs(int length)
+        {
+            if (s_iovs == null || s_iovs.Length < length)
+            {
+                s_iovs = new IOVector[length];
+            }
+            return new Span<IOVector>(s_iovs, 0, length);
+        }
+
+        private static Span<GCHandle> GetHeapHandles(int length)
+        {
+            if (s_gcHandles == null || s_gcHandles.Length < length)
+            {
+                s_gcHandles = new GCHandle[length];
+            }
+            return new Span<GCHandle>(s_gcHandles, 0, length);
+        }
+
+        private unsafe struct IOVector
+        {
+            public IntPtr Base;
+            public IntPtr Length;
+        }
+
+        private unsafe struct msghdr
+        {
+            public IntPtr msg_name; //optional address
+            public uint msg_namelen; //size of address
+            public IOVector* msg_iov; //scatter/gather array
+            public SizeT msg_iovlen; //# elements in msg_iov
+            public void* msg_control; //ancillary data, see below
+            public SizeT msg_controllen; //ancillary data buffer len
+            public int msg_flags; //flags on received message
+        }
+
+        [DllImport ("libc", SetLastError=true)]
+        private static unsafe extern SSizeT sendmsg(int sockfd, msghdr* msg, int flags);
+    }
+}
