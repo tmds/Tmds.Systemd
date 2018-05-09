b98d4f30adb3d5e5c7933eb8bc89d4632a19f0fa
Tom Deseyn <tom.deseyn@gmail.com>
2018-05-09 13:15:17 +0200

    Stringify values using Invariant culture

--- src/Tmds.Systemd/JournalMessage.cs
+++ src/Tmds.Systemd/JournalMessage.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Buffers;
 using System.Buffers.Binary;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -20,6 +22,8 @@ namespace Tmds.Systemd
 
         private readonly List<ArraySegment<byte>> _data;
         private readonly Encoder _encoder;
+        private readonly StringBuilder _stringBuilder;
+        private readonly char[] _charBuffer;
         private byte[] _currentSegment;
         private int _bytesWritten;
 
@@ -27,6 +31,8 @@ namespace Tmds.Systemd
         {
             _data = new List<ArraySegment<byte>>();
             _encoder = s_encoding.GetEncoder();
+            _stringBuilder = new StringBuilder();
+            _charBuffer = new char[1024];
         }
 
         /// <summary>Destructor.</summary>
@@ -55,7 +61,7 @@ namespace Tmds.Systemd
         public void Append(string name, object value)
         {
             // Field name
-            AppendString(name);
+            AppendString(name.AsSpan());
 
             // Separator
             AppendChar('\n');
@@ -66,12 +72,61 @@ namespace Tmds.Systemd
             Span<byte> valueLengthAt = CurrentRemaining;
             _bytesWritten += 8;
             // value
-            int bytesWritten = AppendString(value.ToString());
+            int bytesWritten = AppendObject(value);
             // Fill in length
             BinaryPrimitives.WriteUInt64LittleEndian(valueLengthAt, (ulong)bytesWritten);
 
             // Separator
             AppendChar('\n');
+        }
+
+        private int AppendObject(object value, bool checkEnumerable = true)
+        {
+            int bytesWritten = 0;
+            if (value is string) // Special-case string since it implements IEnumerable
+            {
+                bytesWritten = AppendString(((string)value).AsSpan());
+            }
+            else if (checkEnumerable && value is IEnumerable enumerable)
+            {
+                bool addSeparator = false;
+                foreach (object o in enumerable)
+                {
+                    if (addSeparator)
+                    {
+                        bytesWritten += AppendChar(',');
+                        bytesWritten += AppendChar(' ');
+                    }
+                    bytesWritten += AppendObject(o, checkEnumerable: false);
+                    addSeparator = true;
+                }
+            }
+            else
+            {
+                // StringBuilder leverages an internal ISpanFormattable interface
+                // that avoids allocations on .NET Core 2.1.
+                _stringBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0}", value);
+                bytesWritten += AppendStringBuilder(_stringBuilder);
+                _stringBuilder.Clear();
+            }
+            return bytesWritten;
+        }
+
+        private int AppendStringBuilder(StringBuilder sb)
+        {
+            int bytesWritten = 0;
+            int length = sb.Length;
+            int offset = 0;
+            while (length > 0)
+            {
+                int destinationLength = Math.Min(length, _charBuffer.Length);
+                // .NET Core 2.1 can stackalloc charBuffer
+                sb.CopyTo(offset, _charBuffer, 0,  destinationLength);
+                bytesWritten += AppendString(new Span<char>(_charBuffer, 0, destinationLength));
+                length -= destinationLength;
+                offset += destinationLength;
+            }
+            return bytesWritten;
         }
 
         private void Clear()
@@ -113,7 +168,7 @@ namespace Tmds.Systemd
             }
         }
 
-        private void AppendChar(char c)
+        private int AppendChar(char c)
         {
             if (c <= 127)
             {
@@ -121,14 +176,15 @@ namespace Tmds.Systemd
 
                 _currentSegment[_bytesWritten] = (byte)c;
                 _bytesWritten++;
+                return 1;
             }
             else
             {
-                _AppendMultiByteChar(c);
+                return _AppendMultiByteChar(c);
             }
         }
 
-        private unsafe void _AppendMultiByteChar(char value)
+        private unsafe int _AppendMultiByteChar(char value)
         {
             EnsureCapacity(MaximumBytesPerUtf8Char);
             var destination = CurrentRemaining;
@@ -141,11 +197,11 @@ namespace Tmds.Systemd
             }
 
             _bytesWritten += bytesUsed;
+            return bytesUsed;
         }
 
-        private int AppendString(string s)
+        private int AppendString(ReadOnlySpan<char> buffer)
         {
-            ReadOnlySpan<char> buffer = s.AsSpan();
             int bytesWritten = 0;
 
             while (buffer.Length > 0)
@@ -176,7 +232,7 @@ namespace Tmds.Systemd
             if (_currentSegment == null || (_currentSegment.Length - _bytesWritten) <  + minSize)
             {
                 AppendCurrent();
-                desiredSize = Math.Max(desiredSize, 4096);
+                desiredSize = Math.Max(minSize, Math.Max(desiredSize, 4096));
                 _currentSegment = ArrayPool<byte>.Shared.Rent(minimumLength: desiredSize);
                 _bytesWritten = 0;
             }
