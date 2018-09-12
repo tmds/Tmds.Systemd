5ac9d2dad57d7995af68065b68c1a5f1e98f15d0
Tom Deseyn <tom.deseyn@gmail.com>
2018-09-12 05:25:54 +0200

    Add ServiceManager.{IsRunningAsService,InvocationId}

--- src/Tmds.Systemd/ServiceManager.cs
+++ src/Tmds.Systemd/ServiceManager.cs
@@ -1,10 +1,37 @@
-﻿namespace Tmds.Systemd
+﻿using System;
+
+namespace Tmds.Systemd
 {
     /// <summary>
     /// Interact with the systemd system manager.
     /// </summary>
     public partial class ServiceManager
     {
+        private static string _invocationId;
         private static readonly object _gate = new object();
+
+        /// <summary>
+        /// Returns whether the process is running as part of a unit.
+        /// </summary>
+        public static bool IsRunningAsService => InvocationId != null;
+
+        /// <summary>
+        /// Returns unique identifier of the runtime cycle of the unit.
+        /// </summary>
+        public static string InvocationId
+        {
+            get
+            {
+                if (_invocationId == null)
+                {
+                    _invocationId = Environment.GetEnvironmentVariable("INVOCATION_ID") ?? string.Empty;
+                }
+                if (_invocationId == string.Empty)
+                {
+                    return null;
+                }
+                return _invocationId;
+            }
+        }
     }
 }
